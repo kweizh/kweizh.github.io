58b18d8c4a6bb01e49a02cf2f777016b03452f9e
Wei Zhang <kweizh@tabbyml.com>
2025-12-10 18:13:42 +0800

    Remove basePath and assetPrefix from config

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,7 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/kweizh.github.io' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/kweizh.github.io' : '',
+  }
 };
 
 export default nextConfig;
